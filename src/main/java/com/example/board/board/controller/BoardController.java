ae59220b60f98f6673d6c9efe41d81b5cb36e951
jj-kim4023 <jj.kim4023@gmail.com>
2024-06-18 23:30:17 +0900

    [STUDY] -프로젝트세팅 -게시글작성페이지

---
+++ src/main/java/com/example/board/board/controller/BoardController.java
@@ -0,0 +1,15 @@
+package com.example.board.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+
+    @GetMapping
+    public String saveForm() {
+        return "save";
+    }
+}
---
+++ src/main/java/com/example/board/board/controller/HomeController.java
@@ -0,0 +1,10 @@
+package com.example.board.board.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HomeController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
