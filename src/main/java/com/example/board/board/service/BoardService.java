085bcf1fbf0ea919e66fbfb0f9f0abe4a382c65b
jj-kim4023 <jj.kim4023@gmail.com>
2024-06-20 23:13:09 +0900

    [STUDY] -게시글목록 -게시글조회

--- src/main/java/com/example/board/board/service/BoardService.java
+++ src/main/java/com/example/board/board/service/BoardService.java
@@ -3,8 +3,13 @@ package com.example.board.board.service;
 import com.example.board.board.dto.BoardDTO;
 import com.example.board.board.entity.BoardEntity;
 import com.example.board.board.repository.BoardRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 // DTO -> Entity (Entity Class 에서)
 // Entity -> DTO (DTO Class 에서)
@@ -19,4 +24,38 @@ public class BoardService {
         BoardEntity boardEntity = BoardEntity.toSaveEntity(boardDTO);
         boardRepository.save(boardEntity);
     }
+
+    public List<BoardDTO> findAll() {
+        List<BoardEntity> boardEntityList = boardRepository.findAll();
+        List<BoardDTO> boardDTOList = new ArrayList<>();
+        for (BoardEntity boardEntity : boardEntityList) {
+            boardDTOList.add(BoardDTO.toBoardDTO(boardEntity));
+        }
+        return boardDTOList;
+    }
+
+    @Transactional
+    public void updateHits(Long id) {
+        boardRepository.updateHits(id);
+    }
+
+    public BoardDTO findById(Long id) {
+        Optional<BoardEntity> optionalBoardEntity = boardRepository.findById(id);
+        if (optionalBoardEntity.isPresent()) {
+            BoardEntity boardEntity = optionalBoardEntity.get();
+            BoardDTO boardDTO = BoardDTO.toBoardDTO(boardEntity);
+            return boardDTO;
+        } else {
+            return null;
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
