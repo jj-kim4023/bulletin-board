8d8d3eecf1e4997cc16c8e4e6068f010a6c52693
jj-kim4023 <jj.kim4023@gmail.com>
2024-06-24 23:02:10 +0900

    [STUDY] -게시글 페이징

--- src/main/java/com/example/board/board/controller/BoardController.java
+++ src/main/java/com/example/board/board/controller/BoardController.java
@@ -41,7 +41,8 @@ public class BoardController {
     }
 
     @GetMapping("/{id}")
-    public String findById(@PathVariable(value = "id") Long id, Model model) {
+    public String findById(@PathVariable(value = "id") Long id, Model model,
+                           @PageableDefault(page=1) Pageable pageable) {
         /*
         해당 게시글의 조회를 하나 올리고
         게시글 데이터를 가져와서 detail.html에 출력
@@ -49,6 +50,7 @@ public class BoardController {
         boardService.updateHits(id);
         BoardDTO boardDTO = boardService.findById(id);
         model.addAttribute("board", boardDTO);
+        model.addAttribute("page", pageable.getPageNumber());
         return "detail";
 
     }
@@ -78,6 +80,21 @@ public class BoardController {
     public String paging(@PageableDefault(page = 1) Pageable pageable, Model model) {
 //        pageable.getPageNumber();
         Page<BoardDTO> boardList = boardService.paging(pageable);
-    }
+        int blockLimit = 3;
+        int startPage = (((int)(Math.ceil((double)pageable.getPageNumber() / blockLimit))) - 1) * blockLimit + 1; // 1 4 7 10 ~~
+        int endPage = ((startPage + blockLimit - 1) < boardList.getTotalPages()) ? startPage + blockLimit - 1 : boardList.getTotalPages();
 
+        // page 갯수 20개
+        // 현재 사용자가 3페이지
+        // 1 2 3
+        // 현재 사용자가 7페이지
+        // 7 8 9
+        // 보여지는 페이지 갯수 3개
+        // 총 페이지 갯수 8개라면
+
+        model.addAttribute("boardList", boardList);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        return "paging";
+    }
 }
